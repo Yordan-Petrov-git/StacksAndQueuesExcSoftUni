7dd38a5bfadf2db899fec91434bc12272c272552
Yordan-Petrov-git <y.petrov@ue-varna.bg>
2020-03-21 12:38:17 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader =
+                new BufferedReader(
+                        new InputStreamReader(
+                                System.in
+                        )
+                );
+        //String [] input = reader.readLine().split("\\s+");
+
+        ArrayDeque <String> stack = new ArrayDeque<>();
+
+        Arrays.stream(reader.readLine().split("\\s+")).forEach(stack :: push);
+
+//        for (String s : input) {
+//            stack.push(Integer.parseInt(s));
+//        }
+
+        System.out.println(stack);
     }
 }
